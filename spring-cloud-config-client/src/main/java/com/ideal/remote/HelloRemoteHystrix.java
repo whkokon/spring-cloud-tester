5b67df4583b724806e4af7314c448c7382b68263
杨小清 <yaloo@live.cn>
2017-12-28 19:53:53 +0800

    spring cloud test init

---
+++ spring-cloud-config-client/src/main/java/com/ideal/remote/HelloRemoteHystrix.java
@@ -0,0 +1,12 @@
+package com.ideal.remote;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloRemoteHystrix implements HelloRemote{
+
+  @Override
+  public String hello(String name) {
+    return "hello " +name+", this messge send failed ";
+  }
+}
